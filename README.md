506c2b043c44c13493c34406addb84a1e85144cf
Russell Aaron <russell@webdevstudios.com>
2017-02-17 23:26:33 -0800

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# band-names-cpt
+=== Band Names CPT ===
+Contributors: GeekStreetWP
+Author URI: http://russellenvy.com
+Tags: post, posts, plugin, plugins, admin, custom post type, dashboard, list
+Requires at least: 4.1
+Tested up to: 4.7.2
+Stable tag: 1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
 Simply create a post type called band names and display band names with a shortcode.
+
+== Description ==
+
+Creates a custom post type called band-name. Two shortcodes available. [band-name] & [single-band-name]
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `band-name-cpt.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `band-name-cpt.zip`
+2. Extract the `band-name-cpt` directory to your computer
+3. Upload the `band-name-cpt` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+== Changelog ==
+
+= 1.0 =
+* Release Date - 31st March, 2016
+* First release
---
+++ band-name-plugin.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Plugin Name: Band Names
+ * Plugin URI: http://russellenvy.com
+ * Description: Simply create a post type called band names and display band names with a shortcode.
+ * Version: 1.0
+ * Author: Russell Aaron
+ * Author URI: http://russellenvy.com
+ * Text Domain: band_names
+ * License: GPL2
+ */
+ function band_names_post_type() {
+  $labels = array(
+    'name' => 'Band Names',
+    'singular_name' => 'Band Name',
+    'add_new' => 'Add New Band Name',
+    'add_new_item' => 'Add New Band Name',
+    'edit_item' => 'Edit Band Name',
+    'new_item' => 'New Band Name',
+    'view_item' => 'View Band Name',
+    'search_items' => 'Search Band Names',
+    'not_found' =>  'No Band Names found',
+    'not_found_in_trash' => 'No Band Names found in trash',
+    'parent_item_colon' => '',
+    'menu_name' => 'Band Names'
+  );
+  $args = array(
+    'labels' => $labels,
+    'public' => false,
+    'publicly_queryable' => false,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => false,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false,
+    'menu_position' => null,
+    'supports' => array( 'title' ),
+    'taxonomies' => array( '', ) // this is IMPORTANT
+  ); 
+  register_post_type( 'band-name', $args );
+}
+add_action( 'init', 'band_names_post_type' );
+// add shortcode that displays a loop of cpt posts on a page or post template
+    function get_band_names( $atts) {   
+    $args = array(
+        'post_type' => 'band-name',
+        'posts_per_page' => -1,
+    );
+    ob_start();
+    $band_names = new WP_Query( $args );
+    while ( $band_names->have_posts() ) : $band_names->the_post();  
+    //get post meta info
+    //echo some stuff out
+    echo '<div class="band-name"><h3>'. esc_html( the_title() ) . '</h3></div>';
+    endwhile;
+    wp_reset_query();
+    return ob_get_clean(); 
+    }
+// add shortcode that displays a random cpt posts on a page or post template
+    add_shortcode('band-name', 'get_band_names');
+
+    // add shortcode that displays a random cpt posts on a page or post template
+    function get_single_band_names( $atts) {   
+    $args = array(
+        'post_type' => 'band-name',
+        'posts_per_page' => 1,
+        'orderby' => rand,
+    );
+    ob_start();
+    $single_band_names = new WP_Query( $args );
+    while ( $single_band_names->have_posts() ) : $single_band_names->the_post();  
+    //get post meta info
+    //echo some stuff out
+    echo '<div class="single-band-name"><h3>'. esc_html( the_title() ) . '</h3></div>';
+    endwhile;
+    wp_reset_query();
+    return ob_get_clean(); 
+    }
+// add shortcode that displays a loop of cpt posts on a page or post template
+    add_shortcode('single-band-name', 'get_single_band_names');
+?>
